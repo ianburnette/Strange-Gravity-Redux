cd73e759ad16bf8880d545456e4750a968827c67
Ian Burnette <ian_burnette@yahoo.com>
2015-02-24 16:40:46 -0500

    some top-level pre-expo stuff

---
+++ Assets/masterKeyboardControls.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class masterKeyboardControls : MonoBehaviour {
+
+	NewPauseMenu pauseMenu;
+	
+	void OnLevelWasLoaded(){
+		if (Application.loadedLevel!=0){
+			pauseMenu = GameObject.Find("_gm").GetComponent<NewPauseMenu>();
+		}
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)){ 
+			pauseMenu.TogglePause();
+		}
+		if (Input.GetKeyDown(KeyCode.F1)){ Application.LoadLevel(0);}
+		if (Input.GetKeyDown(KeyCode.F2)){ Application.LoadLevel(Application.loadedLevel);}
+		if (Input.GetKeyDown(KeyCode.F3)){ Application.LoadLevel(1);}
+		if (Input.GetKeyDown(KeyCode.F5)){
+			Application.LoadLevel(0);
+			bool platOnMac = GetComponent<platformInitializer>().onMac;
+			GetComponent<platformInitializer>().onMac = !platOnMac;
+		}
+	}
+}
